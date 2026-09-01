1ba25310c27fcf8ef4dcade8f92b80cfa6abfccb
EmiralpYakali <162481842+EmiralpYakali@users.noreply.github.com>
2026-09-01 11:58:42 +0300

    Normalize flock breeds

---
+++ drizzle/0002_normalize_breeds.sql
@@ -0,0 +1,10 @@
+UPDATE `sheep` SET `breed` = CASE abs(random()) % 6
+  WHEN 0 THEN 'Merinos Anaç'
+  WHEN 1 THEN 'Merinos Erkek'
+  WHEN 2 THEN 'Merinos Dişi'
+  WHEN 3 THEN 'Kıvırcık Anaç'
+  WHEN 4 THEN 'Kıvırcık Erkek'
+  ELSE 'Kıvırcık Dişi'
+END
+WHERE `breed` NOT IN ('Merinos Anaç', 'Merinos Erkek', 'Merinos Dişi', 'Kıvırcık Anaç', 'Kıvırcık Erkek', 'Kıvırcık Dişi');
+
